0381d1b43fcf977da9893de0278946d7a6abbca4
Yusuf Ahmad <yusufahmad5885@gmail.com>
2022-08-05 18:16:12 -0700

    Uploading

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './Playlist.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends React.Component{
+
+    constructor(props){
+        super(props);
+        
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event){
+        this.props.onNameChange(event.target.value);
+    }
+
+    render(){
+        return (
+            <div class="Playlist">
+                <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
+                <TrackList tracks = {this.props.playlistTracks}
+                onRemove = {this.props.onRemove}
+                isRemoval={true}/>
+                <button class="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
+
+export default Playlist;
